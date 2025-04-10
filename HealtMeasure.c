a364361ac2d72e24eaaf539db75c1c1a40f8d7a2
unknown <ie728332@iteso.mx>
2025-04-10 05:23:34 -0600

    switcheo de pantallas fnunciona, adc0 y 1 activo, segundo sensor funciona

--- HealtMeasure.c
+++ HealtMeasure.c
@@ -36,10 +36,16 @@
 #define hello_task_PRIORITY (configMAX_PRIORITIES - 1)
 #define GrapNumb_PRIORITY   (configMAX_PRIORITIES - 2)
 #define X_INCREMENT 1
-#define HM_HEART_ADC16_BASE        ADC1
+
+#define HM_HEART_ADC1_BASE        ADC1
+#define HM_ADC1_CHANNEL_GROUP     0U
+#define HM_ADC1_USER_CHANNEL      23U
+#define HM_ADC1_IRQ_HANDLER_FUNC 		ADC1_IRQHandler
+
+#define HM_HEART_ADC16_BASE        ADC0
 #define HM_ADC16_CHANNEL_GROUP     0U
-#define HM_ADC16_USER_CHANNEL      23U
-#define HM_ADC16_IRQ_HANDLER_FUNC ADC1_IRQHandler
+#define HM_ADC16_USER_CHANNEL      1U
+#define HM_ADC16_IRQ_HANDLER_FUNC ADC0_IRQHandler
 
 typedef struct{
     uint8_t x;
@@ -53,11 +59,17 @@ typedef struct{
 
 TimerHandle_t SendFBTimer;
 TimerHandle_t ADCConversionTimer;
+TimerHandle_t ADCConversionTempTimer;
 adc16_channel_config_t adc16ChannelConfigStruct;
+adc16_channel_config_t adc16ChannelConfigStruct2;
 QueueHandle_t TimeScaleMailbox;
+QueueHandle_t ScreenSelectorMailbox;
 QueueHandle_t AdcConversionQueue;
+QueueHandle_t AdcConversionTempQueue;
 QueueHandle_t PointQueue;
+QueueHandle_t PointTempQueue;
 QueueHandle_t NumberQueue;
+QueueHandle_t NumberTempQueue;
 QueueHandle_t alarmQueue;
 
 /*******************************************************************************
@@ -66,6 +78,7 @@ QueueHandle_t alarmQueue;
 static void LCDprint_thread(void *pvParameters);
 static void GraphProcess_thread(void *pvParameters);
 static void NumberProcess_thread(void *pvParameters);
+static void NumberProcessTemp_thread(void *pvParameters);
 static void Alarm_thread(void *pvParameters);
 
 uint16_t up_limit = 200;
@@ -83,7 +96,7 @@ void ScreenInit(){
     LCD_nokia_clear();
 }
 
-void ADCInit(){
+void ADCInitADC1(){
     adc16_config_t adc16ConfigStruct;
 
     NVIC_SetPriority(ADC1_IRQn,3);
@@ -100,23 +113,57 @@ void ADCInit(){
         PRINTF("ADC16_DoAutoCalibration() Failed.\r\n");
     }
 #endif
-    adc16ChannelConfigStruct.channelNumber = HM_ADC16_USER_CHANNEL;
+    adc16ChannelConfigStruct.channelNumber = HM_ADC1_USER_CHANNEL;
     adc16ChannelConfigStruct.enableInterruptOnConversionCompleted = true; /* Enable the interrupt. */
 #if defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE
     adc16ChannelConfigStruct.enableDifferentialConversion = false;
 #endif /* FSL_FEATURE_ADC16_HAS_DIFF_MODE */
 }
+void ADCInitADC0(){
+    adc16_config_t adc16ConfigStruct;
 
+    NVIC_SetPriority(ADC0_IRQn,3);
+    EnableIRQ(ADC0_IRQn);
 
+    ADC16_GetDefaultConfig(&adc16ConfigStruct);
+    ADC16_Init(ADC0, &adc16ConfigStruct);
+    ADC16_EnableHardwareTrigger(ADC0, false); /* Make sure the software trigger is used. */
+#if defined(FSL_FEATURE_ADC16_HAS_CALIBRATION) && FSL_FEATURE_ADC16_HAS_CALIBRATION
+    if (kStatus_Success == ADC16_DoAutoCalibration(ADC0))
+    {
+        PRINTF("ADC16_DoAutoCalibration() Done.\r\n");
+    }
+    else
+    {
+        PRINTF("ADC16_DoAutoCalibration() Failed.\r\n");
+    }
+#endif
+    adc16ChannelConfigStruct2.channelNumber = HM_ADC16_USER_CHANNEL;
+    adc16ChannelConfigStruct2.enableInterruptOnConversionCompleted = true; /* Enable the interrupt. */
+#if defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE
+    adc16ChannelConfigStruct2.enableDifferentialConversion = false;
+#endif /* FSL_FEATURE_ADC16_HAS_DIFF_MODE */
+}
 void HM_ADC16_IRQ_HANDLER_FUNC(void){
-    uint16_t conversionVal = 0;
+    adcConv_str AdcConv;
+    BaseType_t xHigherPriorityTaskWoken;
+    xHigherPriorityTaskWoken = pdFALSE;
+    AdcConv.data = ADC16_GetChannelConversionValue(HM_HEART_ADC16_BASE, HM_ADC16_CHANNEL_GROUP);
+    xQueueSendFromISR(AdcConversionTempQueue,&AdcConv,&xHigherPriorityTaskWoken);
+    xHigherPriorityTaskWoken = pdFALSE;
+    xQueueSendFromISR(AdcConversionTempQueue,&AdcConv,&xHigherPriorityTaskWoken);
+
+    SDK_ISR_EXIT_BARRIER;
+}
+
+void HM_ADC1_IRQ_HANDLER_FUNC(void){
     adcConv_str AdcConv;
     BaseType_t xHigherPriorityTaskWoken;
 
     xHigherPriorityTaskWoken = pdFALSE;
 
     /* Read conversion result to clear the conversion completed flag. */
-    AdcConv.data = ADC16_GetChannelConversionValue(HM_HEART_ADC16_BASE, HM_ADC16_CHANNEL_GROUP);
+    AdcConv.data = ADC16_GetChannelConversionValue(HM_HEART_ADC1_BASE, HM_ADC1_CHANNEL_GROUP);
     /*Send conversion value to the ADC queue*/
     xQueueSendFromISR(AdcConversionQueue,&AdcConv,&xHigherPriorityTaskWoken);
     /*Restore flag for the ISR-Thread safe purpose*/
@@ -126,6 +173,7 @@ void HM_ADC16_IRQ_HANDLER_FUNC(void){
 
     SDK_ISR_EXIT_BARRIER;
 }
+
 
 void time_scale(uint32_t flags){
 	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
@@ -139,36 +187,60 @@ void time_scale(uint32_t flags){
 	xQueueOverwriteFromISR(TimeScaleMailbox,&x_increment,&xHigherPriorityTaskWoken);
 }
 
+void screen_selector(uint32_t flags){
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+	static uint8_t screen_selection = 1;
+
+	if(screen_selection < 3){
+		screen_selection++;
+	}else{
+		screen_selection = 1;
+	}
+	xQueueOverwriteFromISR(ScreenSelectorMailbox,&screen_selection,&xHigherPriorityTaskWoken);
+}
+
 void SendFBCallback(TimerHandle_t ARHandle){
     LCD_nokia_sent_FrameBuffer();
 }
 
 void ADCConversionCallback(TimerHandle_t ARHandle){
     /*Request ADC conversion*/
-    ADC16_SetChannelConfig(HM_HEART_ADC16_BASE, HM_ADC16_CHANNEL_GROUP, &adc16ChannelConfigStruct);
+    ADC16_SetChannelConfig(HM_HEART_ADC1_BASE, HM_ADC1_CHANNEL_GROUP, &adc16ChannelConfigStruct);
 }
-
+void ADCConversionTempCallback(TimerHandle_t ARHandle){
+    ADC16_SetChannelConfig(HM_HEART_ADC16_BASE,HM_ADC16_CHANNEL_GROUP , &adc16ChannelConfigStruct2);
+}
 int main(void){
     uint8_t init_time_scale = 1;
+    uint8_t init_screen = 1;
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
 
     ScreenInit();
-    ADCInit();
+    ADCInitADC1();
+    ADCInitADC0();
 
     init_button(sw3);
+    init_button(sw2);
     NVIC_set_basepri_threshold(PRIORITY_10);
     NVIC_enable_interrupt_and_priotity(PORTA_IRQ,PRIORITY_4);
+    NVIC_enable_interrupt_and_priotity(PORTD_IRQ,PRIORITY_4);
     NVIC_global_enable_interrupts;
 
     set_button_as_interrupt(sw3);
+    set_button_as_interrupt(sw2);
     GPIO_callback_init(GPIO_A, time_scale);
+    GPIO_callback_init(GPIO_D, screen_selector);
 
     AdcConversionQueue = xQueueCreate(5,sizeof(adcConv_str));
+    AdcConversionTempQueue = xQueueCreate(5,sizeof(adcConv_str));
     PointQueue = xQueueCreate(5,sizeof(uint16_t));
+    PointTempQueue = xQueueCreate(5,sizeof(uint16_t));
     NumberQueue = xQueueCreate(5,sizeof(uint16_t));
+    NumberTempQueue = xQueueCreate(5,sizeof(uint16_t));
     TimeScaleMailbox = xQueueCreate(1,sizeof(uint8_t));
+    ScreenSelectorMailbox = xQueueCreate(1,sizeof(uint8_t));
     alarmQueue = xQueueCreate(1,sizeof(uint8_t));
 
     SendFBTimer = xTimerCreate(
@@ -184,12 +256,15 @@ int main(void){
             pdTRUE, /*Auto-reload mode*/
             0, /*Timer's ID. Not used*/
             ADCConversionCallback); /*Auto-Reload notification function. Called at expiration time*/
+    ADCConversionTempTimer = xTimerCreate("ADCConversionTemp",pdMS_TO_TICKS(100),pdTRUE,0, ADCConversionTempCallback);
     /*Start FrameBuffer update timer*/
     xTimerStart(SendFBTimer,0);
     /*Start ADC measurement timer*/
     xTimerStart(ADCConversionTimer,0);
+    xTimerStart(ADCConversionTempTimer,0);
     /*Publish time-scale init value*/
     xQueueOverwrite(TimeScaleMailbox,&init_time_scale);
+    xQueueOverwrite(ScreenSelectorMailbox,&init_screen);
 
     if (xTaskCreate(LCDprint_thread, "LCDprint_thread", configMINIMAL_STACK_SIZE + 100, NULL, hello_task_PRIORITY, NULL) !=pdPASS){
         PRINTF("LCDprint_thread creation failed!.\r\n");
@@ -203,14 +278,17 @@ int main(void){
         PRINTF("NumberProcess_thread creation failed!.\r\n");
         while (1);
     }
+    if (xTaskCreate(NumberProcessTemp_thread, "NumberProcessTemp_thread", configMINIMAL_STACK_SIZE + 100, NULL, GrapNumb_PRIORITY, NULL) !=pdPASS){
+        PRINTF("NumberProcessTemp_thread creation failed!.\r\n");
+        while (1);
+    }
     if (xTaskCreate(Alarm_thread, "Alarm_thread", configMINIMAL_STACK_SIZE + 100, NULL, GrapNumb_PRIORITY, NULL) !=pdPASS){
         PRINTF("Alarm_thread creation failed!.\r\n");
         while (1);
-        }
+    }
     vTaskStartScheduler();
     for (;;);
 }
-
 
 static void Alarm_thread(void *pvParameters){
     uint16_t alarm_value;
@@ -218,7 +296,6 @@ static void Alarm_thread(void *pvParameters){
     static uint16_t count_ss = 0;
     static uint16_t count_up_limit = 0;
     static uint16_t count_inferior_limit = 0;
-
     for (;;){
         /*Wait for the ADC conversion to be avaliable*/
         if(xQueueReceive(NumberQueue,&alarm_value ,pdMS_TO_TICKS(5)) != errQUEUE_EMPTY){
@@ -259,80 +336,159 @@ static void LCDprint_thread(void *pvParameters){
     uint8_t onedigit = 0;
     uint8_t secdigit = 0;
     uint8_t thrdigit = 0;
+    point_str pointTemp_a = {0};
+    point_str pointTemp_b = {0};
+    uint16_t ypointGraphTemp;
+    uint16_t ypointNumberTemp;
+    uint8_t onedigitTemp = 0;
+    uint8_t secdigitTemp = 0;
+    uint8_t thrdigitTemp = 0;
+    uint8_t screen_selected;
     static uint8_t set_alarm = 0;
 
     for (;;){
-        /*Received ADC processed value to the graphic scale*/
-        if(xQueueReceive(PointQueue,&ypointGraph, pdMS_TO_TICKS(5)) != errQUEUE_EMPTY){
-            /*Read the time-scale currently configured*/
-            if(xQueuePeek(TimeScaleMailbox,&x_increment,pdMS_TO_TICKS(5))){
-                /*Get next point to be graphed*/
-                point_a = point_b;
-                point_b.y = ypointGraph;
-                /*Check if next point is range*/
-                if(point_b.x<84){
-                    point_b.x += x_increment;
-                }
-                /*If not in range, clear the graph zone and start in 0*/
-                else{
-                    point_b.x = 0;
-                    LCD_nokia_clear_range_FrameBuffer(0,3,252);
-                }
-                /*Draw a line between the old and new points and draw 50 points to complete the line*/
-                drawline(point_a.x,point_a.y,point_b.x,point_b.y,50);
-            }
-        }
-        /*Received ADC processed value for the numeric scale*/
-        if(xQueueReceive(NumberQueue,&ypointNumber, pdMS_TO_TICKS(5)) != errQUEUE_EMPTY){
-            LCD_nokia_clear_range_FrameBuffer(0,1,20);
-            onedigit = ((ypointNumber/100)%10)+0x30;
-            secdigit = ((ypointNumber/10)%10)+0x30;
-            thrdigit = (ypointNumber%10)+0x30;
-            LCD_nokia_write_char_xy_FB(0,1,onedigit);
-            LCD_nokia_write_char_xy_FB(5,1,secdigit);
-            LCD_nokia_write_char_xy_FB(10,1,'.');
-            LCD_nokia_write_char_xy_FB(15,1,thrdigit);
-            LCD_nokia_write_char_xy_FB(20,1,' ');
-            LCD_nokia_write_string_xy_FB(25,1,"mv");
-            taskYIELD();
-        }
-        if(xQueueReceive(alarmQueue,&set_alarm, pdMS_TO_TICKS(5)) != errQUEUE_EMPTY){
-        	if (set_alarm){
-        		LCD_nokia_write_string_xy_FB(1,2,"ALARM");
+    	if(xQueuePeek(ScreenSelectorMailbox,&screen_selected,pdMS_TO_TICKS(5))){
+			/*Received ADC processed value to the graphic scale*/
+			if(xQueueReceive(PointQueue,&ypointGraph, pdMS_TO_TICKS(5)) != errQUEUE_EMPTY){
+				/*Read the time-scale currently configured*/
+				if(xQueuePeek(TimeScaleMailbox,&x_increment,pdMS_TO_TICKS(5))){
+					/*Get next point to be graphed*/
+					point_a = point_b;
+					point_b.y = ypointGraph;
+						/*Check if next point is range*/
+					if(point_b.x<84){
+						point_b.x += x_increment;
+					}
+					/*If not in range, clear the graph zone and start in 0*/
+					else{
+						point_b.x = 0;
+						LCD_nokia_clear_range_FrameBuffer(0,3,252);
+					}
+					/*Draw a line between the old and new points and draw 50 points to complete the line*/
+					//drawline(point_a.x,point_a.y,point_b.x,point_b.y,50);
+				}
+			}
+			if(xQueueReceive(alarmQueue,&set_alarm, pdMS_TO_TICKS(5)) != errQUEUE_EMPTY){
+				if (set_alarm){
+					LCD_nokia_write_string_xy_FB(1,2,"ALARM");
+				}
+				else{
+					LCD_nokia_write_string_xy_FB(1,2,"     ");
+				}
+			}
+		if(xQueueReceive(PointTempQueue,&ypointGraphTemp, pdMS_TO_TICKS(5)) != errQUEUE_EMPTY){
+				if(xQueuePeek(TimeScaleMailbox,&x_increment,pdMS_TO_TICKS(5))){
+					pointTemp_a = pointTemp_b;
+					pointTemp_b.y = ypointGraphTemp;
+					if(pointTemp_b.x<84){
+						pointTemp_b.x += x_increment;
+					}
+					else{
+						pointTemp_b.x = 0;
+						LCD_nokia_clear_range_FrameBuffer(0,3,252);
+					}
+				}
+			}
+    		if (screen_selected == 1){
+				drawline(point_a.x,point_a.y,point_b.x,point_b.y,50);
+				if(xQueueReceive(NumberQueue,&ypointNumber, pdMS_TO_TICKS(5)) != errQUEUE_EMPTY){
+					LCD_nokia_clear_range_FrameBuffer(0,1,20);
+					onedigit = ((ypointNumber/100)%10)+0x30;
+					secdigit = ((ypointNumber/10)%10)+0x30;
+					thrdigit = (ypointNumber%10)+0x30;
+					LCD_nokia_write_char_xy_FB(0,1,onedigit);
+					LCD_nokia_write_char_xy_FB(5,1,'.');
+					LCD_nokia_write_char_xy_FB(10,1,secdigit);
+					LCD_nokia_write_char_xy_FB(15,1,thrdigit);
+					LCD_nokia_write_char_xy_FB(20,1,' ');
+					LCD_nokia_write_string_xy_FB(25,1,"mv");
+					taskYIELD();
+				}
+    		}
+        	if (screen_selected == 2 ){
+				drawline(pointTemp_a.x,pointTemp_a.y,pointTemp_b.x,pointTemp_b.y,50);
+				if(xQueueReceive(NumberTempQueue,&ypointNumberTemp, pdMS_TO_TICKS(5)) != errQUEUE_EMPTY){
+					LCD_nokia_clear_range_FrameBuffer(0,1,20);
+					onedigitTemp = ((ypointNumberTemp/100)%10)+0x30;
+					secdigitTemp = ((ypointNumberTemp/10)%10)+0x30;
+					thrdigitTemp = (ypointNumberTemp%10)+0x30;
+					LCD_nokia_write_char_xy_FB(0,1,onedigitTemp);
+					LCD_nokia_write_char_xy_FB(5,1,secdigitTemp);
+					LCD_nokia_write_char_xy_FB(10,1,'.');
+					LCD_nokia_write_char_xy_FB(15,1,thrdigitTemp);
+					LCD_nokia_write_char_xy_FB(20,1,' ');
+					LCD_nokia_write_string_xy_FB(25,1,"°");
+					taskYIELD();
+				}
         	}
-        	else{
-        		LCD_nokia_write_string_xy_FB(1,2,"     ");
-        	}
+    		if (screen_selected == 3){
+    			static uint8_t initial_clean = 1;
+    			if(initial_clean){
+					LCD_nokia_clear_range_FrameBuffer(0,3,252);
+					LCD_nokia_clear_range_FrameBuffer(0,1,20);
+					initial_clean = 0;
+    			}
+				drawline(point_a.x,(point_a.y)+15,point_b.x,(point_b.y)+15,50);
+				drawline(pointTemp_a.x,pointTemp_a.y,pointTemp_b.x,pointTemp_b.y,50);
+
+    		}
+
         }
     }
 }
-
-
 static void GraphProcess_thread(void *pvParameters){
     adcConv_str adcConvVal;
+    adcConv_str adcConvVal2;
     uint16_t processedVal;
+    uint16_t processedVal2;
+    uint8_t screen_selected;
     for (;;){
+    	if(xQueuePeek(ScreenSelectorMailbox,&screen_selected,pdMS_TO_TICKS(5))){
+    		if (screen_selected == 1 || screen_selected == 3){
         /*Wait for the ADC conversion to be avaliable*/
-        if(xQueueReceive(AdcConversionQueue,&adcConvVal,portMAX_DELAY) != errQUEUE_EMPTY){
-            /*Process the raw adc value to be in range from 0 to 43*/
-            processedVal = ((adcConvVal.data * 6)/1025);
-            xQueueSend(PointQueue,&processedVal,portMAX_DELAY);
-            taskYIELD();
+				if(xQueueReceive(AdcConversionQueue,&adcConvVal,portMAX_DELAY) != errQUEUE_EMPTY){
+					/*Process the raw adc value to be in range from 0 to 43*/
+					processedVal = ((adcConvVal.data * 6)/1025);
+					xQueueSend(PointQueue,&processedVal,portMAX_DELAY);
+					taskYIELD();
+				}
+    		}  //screen 2 option ECG
+    		if (screen_selected == 2 || screen_selected == 3){
+    			if(xQueueReceive(AdcConversionTempQueue,&adcConvVal2,portMAX_DELAY) != errQUEUE_EMPTY){
+					processedVal2 = ((adcConvVal2.data * 6)/1025);
+					xQueueSend(PointTempQueue,&processedVal2,portMAX_DELAY);
+					taskYIELD();
+    			}
+    		}
         }
     }
 }
-
 static void NumberProcess_thread(void *pvParameters){
     adcConv_str adcConvVal;
     uint16_t processedVal;
     for (;;){
         /*Wait for the ADC conversion to be avaliable*/
         if(xQueueReceive(AdcConversionQueue,&adcConvVal,portMAX_DELAY) != errQUEUE_EMPTY){
-            /*Process the raw adc value to be in range from 0 to 327 (3.27mv)*/
-            processedVal = ((adcConvVal.data * 8)/100);
+            /*Process the raw adc value to be in range from 0 to 300 (3.0023mv)*/
+            processedVal = ((adcConvVal.data * 7.33)/100);
             xQueueSend(NumberQueue,&processedVal,portMAX_DELAY);
             //send twice to read again
             xQueueSend(NumberQueue,&processedVal,portMAX_DELAY);
+            taskYIELD();
+        }
+    }
+}
+static void NumberProcessTemp_thread(void *pvParameters){
+    adcConv_str adcConvVal;
+    uint16_t processedVal;
+    for (;;){
+        /*Wait for the ADC conversion to be avaliable*/
+        if(xQueueReceive(AdcConversionTempQueue,&adcConvVal,portMAX_DELAY) != errQUEUE_EMPTY){
+            /*Process the raw adc value to be in range from 340 to 400 (34 - 40)*/
+            processedVal = 340+((adcConvVal.data * 60)/4096);
+            xQueueSend(NumberTempQueue,&processedVal,portMAX_DELAY);
+            //send twice to read again
+            xQueueSend(NumberTempQueue,&processedVal,portMAX_DELAY);
             taskYIELD();
         }
     }
